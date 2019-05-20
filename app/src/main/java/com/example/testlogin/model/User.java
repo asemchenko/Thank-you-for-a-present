dd297605994cf0e1ba8c70073a42aae42d5d1f8a
Zhenia Koshovets <geka2049@gmail.com>
2019-05-20 15:32:27 +0300

    added News Feed

---
+++ app/src/main/java/com/example/testlogin/model/User.java
@@ -0,0 +1,33 @@
+package com.example.testlogin.model;
+
+public class User {
+    private String name;
+    private String nick;
+    private String imageURL;
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+
+}
